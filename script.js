acd527ef475d6d894faf78bc7d453adf2fcd1c9c
Isaac Garcia <garciaalejoisaac@gmail.com>
2024-08-20 14:47:28 -0500

    advantage

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 /* Primer Ejercicio*/
 
-let edad = parseInt(prompt("Digite su edad  : "))
+ let edad = parseInt(prompt("Digite su edad  : "))
 
 if(edad > 18){
     console.log("Usted es mayor de edad : " + edad)
 }else{
     console.log("Usted es menor de edad" + edad)
+} 
+
+/* Segundo Ejercicio*/
+
+let num = parseInt(prompt(" Ingresa el numeró"))
+
+if(num > 0 ){
+    console.log(" El numero es positivo " + num)
+}else{
+    console.log(" El numero es negativo " + num)
 }
+
+/* Tercero Ejercicio*/
+
