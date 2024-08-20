d27f83236e8e4c4582748d0c13e20635d549bd5d
Isaac Garcia <garciaalejoisaac@gmail.com>
2024-08-20 15:47:31 -0500

    new

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
-/* Primer Ejercicio*/
-
- let edad = parseInt(prompt("Digite su edad  : "))
-
-if(edad > 18){
-    console.log("Usted es mayor de edad : " + edad)
-}else{
-    console.log("Usted es menor de edad" + edad)
-} 
 
 /* Segundo Ejercicio*/
 
- let num = parseInt(prompt(" Ingresa el numeró"))
+/* let num = parseInt(prompt(" Ingresa el numeró"))
 
 if(num > 0 ){
     console.log(" El numero es positivo " + num)
 }else{
     console.log(" El numero es negativo " + num)
-} 
+} */
 
 /* Tercero Ejercicio*/
 
- let numero;
+/* let numero;
 numero = prompt("Introduzca un número : ")
 if(numero % 2 != 0){
     console.log("El numero " + numero + " es impar");
 }else{
     console.log("El numero " + numero + " es par");
-} 
+} */
 
 /* Octavo  Ejercicio*/
 
-let menu = prompt("Ingresa tu numero de pedido : ")
+/* let menu = prompt("Ingresa tu numero de pedido : ")
 switch(menu){
     case "1":
         console.log("Pizza")
@@ -43,11 +34,11 @@ switch(menu){
         break;
     default:
         console.log("Opción no valida")            
-} 
+} */
 
 /* duodecimo ejercicio*/
 
- let año;
+/* let año;
 
 año = parseInt(prompt("Ingresa un año : "))
 if((año % 4 == 0) && (año % 100 != 0)){
@@ -58,5 +49,25 @@ if((año % 4 == 0) && (año % 100 != 0)){
     }else{
         console.log("El año "+ año + " no es bisiesto");
     }
-} 
+} */
 
+ /* séptimo ejercicio*/
+
+ // Solicita al usuario que ingrese una nota entre 0 y 100
+let nota = parseInt(prompt("Ingresa una nota entre 0 y 100:"));
+
+// Verifica si la nota está dentro del rango permitido
+if (isNaN(nota) || nota < 0 || nota > 100) {
+    console.log("Por favor, ingresa una nota válida entre 0 y 100.");
+} else {
+    // Muestra la clasificación basada en la nota
+    if (nota >= 90) {
+        console.log("Excelente");
+    } else if (nota >= 70) {
+        console.log("Bueno");
+    } else if (nota >= 50) {
+        console.log("Regular");
+    } else {
+        console.log("Insuficiente");
+    }
+}
