cade4a3ea8ef1a239d4d5c4e9a8a0e12acb44422
Isaac Garcia <garciaalejoisaac@gmail.com>
2024-08-20 15:21:38 -0500

    new

--- script.js
+++ script.js
@@ -10,13 +10,53 @@ if(edad > 18){
 
 /* Segundo Ejercicio*/
 
-let num = parseInt(prompt(" Ingresa el numeró"))
+ let num = parseInt(prompt(" Ingresa el numeró"))
 
 if(num > 0 ){
     console.log(" El numero es positivo " + num)
 }else{
     console.log(" El numero es negativo " + num)
-}
+} 
 
 /* Tercero Ejercicio*/
+
+ let numero;
+numero = prompt("Introduzca un número : ")
+if(numero % 2 != 0){
+    console.log("El numero " + numero + " es impar");
+}else{
+    console.log("El numero " + numero + " es par");
+} 
+
+/* Octavo  Ejercicio*/
+
+let menu = prompt("Ingresa tu numero de pedido : ")
+switch(menu){
+    case "1":
+        console.log("Pizza")
+        break;
+    case "2": 
+        console.log("Hamburguesa")
+        break;
+    case "3": 
+        console.log("Tacos")
+        break;
+    default:
+        console.log("Opción no valida")            
+} 
+
+/* duodecimo ejercicio*/
+
+ let año;
+
+año = parseInt(prompt("Ingresa un año : "))
+if((año % 4 == 0) && (año % 100 != 0)){
+    console.log("El año " + año +" es bisiesto")
+}else{
+    if((año % 100 == 0) && (año % 400 == 0)){
+        console.log("El año " + año + " es bisiesto");
+    }else{
+        console.log("El año "+ año + " no es bisiesto");
+    }
+} 
 
